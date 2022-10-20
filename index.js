8f57cf990b86974dda4b0f84b6f09c07febbce7b
jinnurain-jihan <zinnurainzihan171@gmail.com>
2022-10-20 18:30:35 +0600

    html element

---
+++ index.js
@@ -0,0 +1,21 @@
+// getElementById
+// var h1=document.getElementById("heading1");
+// h1.innerHTML="bangladesh";
+// var h2=document.getElementById("heading2");
+// h2.innerHTML="Goodbye Goodbye";
+// var p=document.getElementById("pera");
+// p.innerHTML="i love bangladesh";
+
+
+// getElementsByTagName
+// var myHeading1=document.getElementsByTagName("h1")[0];
+// myHeading1.innerHTML="bangladesh";
+// var myHeading2=document.getElementsByTagName("h1")[1];
+// myHeading2.innerHTML="i love bangladesh";
+
+
+//getElementsByClassName
+var h1=document.getElementsByClassName("heading")[0];
+h1.innerHTML="bangladesh";
+var h2=document.getElementsByClassName("heading")[1];
+h2.innerHTML="i love bangladesh";
